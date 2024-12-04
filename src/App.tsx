bc6f640e7e6dd31dca586c0e2da0b1d21a0cdf10
Sara Montagud Rodríguez <saramontagudrodriguez@MacBook-Air-de-Sara.local>
2024-12-04 18:17:57 +0100

    fix: select between icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,33 @@ import { useState } from "react";
 import "./App.css";
 import Layout from "./layouts/Layout";
 import { Bicycle } from "./assets/icons/Bicycle";
+import { HomeIcon } from "./assets/icons/Home";
+import { SearchIcon } from "./assets/icons/Search";
+import { SettingsIcon } from "./assets/icons/Settings";
+
+// Mapeo de iconos
+const iconsMap = {
+  Bicycle: Bicycle,
+  Home: HomeIcon,
+  Search: SearchIcon,
+  Settings: SettingsIcon
+};
 
 function App() {
-  const [icon, setIcon] = useState("Bicycle");
+  const [icon, setIcon] = useState<keyof typeof iconsMap>("Bicycle");
+
+  const IconComponent = iconsMap[icon];
+
 
   return (
     <Layout>
-      <p>Hola</p>
-      <Bicycle strokeWidth={1} />
+      <IconComponent strokeWidth={1} width={50} height={50} />
+      <div className="flex gap-4">
+        <button onClick={() => setIcon("Home")}>Home Icon</button>
+        <button onClick={() => setIcon("Search")}>Search Icon</button>
+        <button onClick={() => setIcon("Bicycle")}>Bicycle Icon</button>
+        <button onClick={() => setIcon("Settings")}>Settings Icon</button>
+      </div>
     </Layout>
   );
 }
