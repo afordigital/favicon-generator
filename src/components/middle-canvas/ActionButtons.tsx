5cf86898f590f60cd3cf8a42a27bb98c718d57df
LauraNicole449 <lauranicolegarcia449@gmail.com>
2024-12-17 12:54:07 +0100

    change icons undo redo bien

--- src/components/middle-canvas/ActionButtons.tsx
+++ src/components/middle-canvas/ActionButtons.tsx
@@ -1,6 +1,7 @@
 import { IconContext } from "@/App";
 import { useContext } from "react";
 import { Button } from "../ui/button";
+import { Undo, Redo } from "lucide-react";
 
 export const ActionButtons = () => {
   const { undo, redo, canUndo, canRedo } = useContext(IconContext);
@@ -13,8 +14,7 @@ export const ActionButtons = () => {
         variant="outline"
         disabled={!canUndo}
       >
-        
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-undo"><path d="M3 7v6h6"/><path d="M21 17a9 9 0 0 0-9-9 9 9 0 0 0-6 2.3L3 13"/></svg>
+      <Undo />
         Undo
       </Button>
       <Button
@@ -23,9 +23,10 @@ export const ActionButtons = () => {
         variant="outline"
         disabled={!canRedo}
       >
+        
         Redo
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-redo"><path d="M21 7v6h-6"/><path d="M3 17a9 9 0 0 1 9-9 9 9 0 0 1 6 2.3l3 2.7"/></svg>
-      </Button>
+        <Redo />     
+        </Button>
     </div>
   );
 };
