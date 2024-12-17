e3d9a14b0e912b1d48cde2aaf304d2610609d4f6
LauraNicole449 <lauranicolegarcia449@gmail.com>
2024-12-17 12:28:12 +0100

    a1

--- src/components/middle-canvas/ActionButtons.tsx
+++ src/components/middle-canvas/ActionButtons.tsx
@@ -18,7 +18,7 @@ export const ActionButtons = () => {
         Undo
       </Button>
       <Button
-        className="px-6 py-2border-2 border-white border-solid hover:bg-neutral-800"
+        className="px-6 py-2 border-2 border-white border-solid hover:bg-neutral-800"
         onClick={redo}
         variant="outline"
         disabled={!canRedo}
