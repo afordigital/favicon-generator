71dd6963a086d047a99e4cf8995eb87ced0c7909
JesusP2 <jesus_perez99@hotmail.com>
2024-12-19 17:16:42 -0600

    added noise to the background

--- src/components/middle-canvas/Canvas.tsx
+++ src/components/middle-canvas/Canvas.tsx
@@ -10,8 +10,6 @@ export const Canvas = () => {
   const ICON_X = icon.iconSize ? CANVAS_SIZE / 2 - icon.iconSize / 2 + (icon.xOffset ?? 0) : 0 + (icon.xOffset ?? 0);
   const ICON_Y = icon.iconSize ? CANVAS_SIZE / 2 - icon.iconSize / 2 + (icon.yOffset ?? 0) : 0 + (icon.yOffset ?? 0);
 
-  console.log(icon);
-
   return (
     <>
       <svg
@@ -22,27 +20,42 @@ export const Canvas = () => {
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
       >
-        {/* <filter id="multiLayerNoiseFilter">
-          <feTurbulence type="fractalNoise" baseFrequency={1.73} numOctaves="3" stitchTiles="stitch" result="noise1" />
-          <feColorMatrix
-            in="noise1"
-            type="matrix"
-            values={`0 0 0 0 0
-                 0 0 0 0 0
-                 0 0 0 0 0
-                 0 0 0 ${icon.noiseOpacity} 0`}
-            result="colorNoise"
+        <defs>
+          <filter id="noiseFilter">
+            <feTurbulence type="fractalNoise" baseFrequency="1" numOctaves="2" result="noise" />
+            <feColorMatrix in="noise" type="matrix" values="1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 1 0" result="grayscaleNoise" />
+            <feComponentTransfer in="grayscaleNoise" result="thresholdedNoise">
+              <feFuncR type="discrete" tableValues="0 1" />
+              <feFuncG type="discrete" tableValues="0 1" />
+              <feFuncB type="discrete" tableValues="0 1" />
+              <feFuncA type="discrete" tableValues="0 1" />
+            </feComponentTransfer>
+            <feFlood flood-color="white" result="white" />
+            <feComposite in="white" in2="thresholdedNoise" operator="in" result="whiteNoise" />
+            <feBlend in2="SourceGraphic" mode="over" in="whiteNoise" />
+          </filter>
+
+          <mask id="roundMask">
+            <rect
+              width={CANVAS_SIZE}
+              height={CANVAS_SIZE}
+              fill="white"
+              rx={icon.radius}
+              ry={icon.radius}
+            />
+          </mask>
+        </defs>
+
+        <g mask="url(#roundMask)">
+          <rect
+            width={CANVAS_SIZE}
+            height={CANVAS_SIZE}
+            fill={icon.bgColor}
+            rx={icon.radius}
+            ry={icon.radius}
+            filter="url(#noiseFilter)"
           />
-        </filter> */}
-        <rect
-          width={CANVAS_SIZE}
-          height={CANVAS_SIZE}
-          style={{ fill: icon.bgColor }}
-          rx={icon.radius}
-          ry={icon.radius}
-          filter="url(#multiLayerNoiseFilter)"
-          opacity={icon.noiseTexture ? icon.noiseOpacity : 0}
-        />
+        </g>
 
         <Icon
           x={ICON_X}
