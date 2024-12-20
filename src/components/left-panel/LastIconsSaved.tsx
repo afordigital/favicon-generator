d049be0bd8cd5d4f98aefb0349c6c65d13b2a98f
Sara Montagud Rodríguez <saramontagudrodriguez@MacBook-Air-de-Sara.local>
2024-12-20 10:46:50 +0100

    fix: solve ts errors

--- src/components/left-panel/LastIconsSaved.tsx
+++ src/components/left-panel/LastIconsSaved.tsx
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
 import Icon from '../icon';
 import { CollapsibleComponent } from './Collapsible';
-import { IconContext } from '@/App';
+import { IconContext, IconProps } from '@/App';
 
-export const LastIconsSaved = ({ lastIcons }: { lastIcons: string }) => {
+export const LastIconsSaved = ({ lastIcons }: { lastIcons: IconProps[] }) => {
   const { setSvgElement, setIcon } = useContext(IconContext);
 
   const CANVAS_SIZE = 44;
@@ -13,18 +13,24 @@ export const LastIconsSaved = ({ lastIcons }: { lastIcons: string }) => {
     <CollapsibleComponent title="Last Icons">
       <div className="grid mt-4 grid-cols-[repeat(6,1fr)] gap-2">
         {lastIcons.map((lastIcon) => {
+          const iconSize = lastIcon.iconSize ?? CANVAS_CONTAINER_SIZE; 
+          const xOffset = lastIcon.xOffset ?? 0;
+          const yOffset = lastIcon.yOffset ?? 0;
+
+          
           const ICON_X =
-            ((lastIcon.iconSize
-              ? CANVAS_CONTAINER_SIZE / 2 - lastIcon.iconSize / 2 + (lastIcon.xOffset ?? 0)
-              : 0 + (lastIcon.xOffset ?? 0)) *
-              CANVAS_SIZE) /
-            CANVAS_CONTAINER_SIZE;
-          const ICON_Y =
-            ((lastIcon.iconSize
-              ? CANVAS_CONTAINER_SIZE / 2 - lastIcon.iconSize / 2 + (lastIcon.yOffset ?? 0)
-              : 0 + (lastIcon.yOffset ?? 0)) *
-              CANVAS_SIZE) /
-            CANVAS_CONTAINER_SIZE;
+          ((iconSize
+            ? CANVAS_CONTAINER_SIZE / 2 - iconSize / 2 + xOffset
+            : 0 + xOffset) *
+            CANVAS_SIZE) /
+          CANVAS_CONTAINER_SIZE;
+
+        const ICON_Y =
+          ((iconSize
+            ? CANVAS_CONTAINER_SIZE / 2 - iconSize / 2 + yOffset
+            : 0 + yOffset) *
+            CANVAS_SIZE) /
+          CANVAS_CONTAINER_SIZE;
 
           return (
             <button
@@ -43,19 +49,19 @@ export const LastIconsSaved = ({ lastIcons }: { lastIcons: string }) => {
                 <rect
                   width={CANVAS_SIZE}
                   height={CANVAS_SIZE}
-                  style={{ fill: lastIcon.bgColor }}
+                  style={{ fill: lastIcon.primaryBgColor }}
                   rx={(lastIcon.radius * CANVAS_SIZE) / CANVAS_CONTAINER_SIZE}
                   ry={(lastIcon.radius * CANVAS_SIZE) / CANVAS_CONTAINER_SIZE}
                 />
 
-                <Icon
+            <Icon
                   x={ICON_X}
                   y={ICON_Y}
                   name={lastIcon.iconName}
-                  color={lastIcon.iconColor}
+                  color={lastIcon.color}
                   strokeWidth={1}
-                  width={(lastIcon.iconSize * CANVAS_SIZE) / CANVAS_CONTAINER_SIZE}
-                  height={(lastIcon.iconSize * CANVAS_SIZE) / CANVAS_CONTAINER_SIZE}
+                  width={(iconSize * CANVAS_SIZE) / CANVAS_CONTAINER_SIZE}
+                  height={(iconSize * CANVAS_SIZE) / CANVAS_CONTAINER_SIZE}
                 />
               </svg>
             </button>
