0950722f897a0d001291c143ba9f401ca3f45448
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-12-25 21:15:53 +0100

    update layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import { Header } from '@/components/header/Header';
 import { LeftSidebar } from '@/components/left-panel/Sidebar';
 import { RightSidebar } from '@/components/right-panel/Sidebar';
 import { ThemeProvider } from '@/components/theme-provider';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -18,7 +18,7 @@ export default function Layout({ children }: LayoutProps) {
             <LeftSidebar />
             <SidebarTrigger className="text-white mt-2 p-6 border rounded-[4px] border-solid" />
 
-            <main className="flex w-full text-white">{children}</main>
+            <SidebarInset className="flex w-full text-white">{children}</SidebarInset>
 
             <SidebarProvider className="w-fit">
               <SidebarTrigger className="text-white mt-2 p-6 border rounded-[4px] border-solid" />
